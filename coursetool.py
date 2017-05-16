9d9818b2f6d20be49f491e67420137c1fbfdf54a
Tin Pavlinic <tpavlinic@simplemachines.com.au>
2017-05-16 23:18:25 +1000

    Loop over the branches making commits

--- coursetool.py
+++ coursetool.py
@@ -5,6 +5,12 @@ def load_settings(path):
     return yaml.load(open(path))
 
 def write_to(repo, branches, target):
+
+    current_parent = []
+
     for branch in branches:
-        branch_commit = repo.commit(branch)
-        print(branch_commit)
+        branch_head_tree = repo.commit(branch).tree
+        commit_for_branch = git.Commit.from_tree(repo, branch_head_tree, "Commit from " + branch, current_parent)
+        current_parent = [commit_for_branch]
+
+    print(current_parent)    
