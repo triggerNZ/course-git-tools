720e33bf8a4e5eeebbfef944b6b5fa19b3f00036
Tin Pavlinic <tpavlinic@simplemachines.com.au>
2017-05-16 23:28:11 +1000

    creating branch

--- coursetool.py
+++ coursetool.py
@@ -4,13 +4,13 @@ import git
 def load_settings(path):
     return yaml.load(open(path))
 
-def write_to(repo, branches, target):
-
+def write_to(repo, branches, target_branch):
     current_parent = []
 
     for branch in branches:
         branch_head_tree = repo.commit(branch).tree
-        commit_for_branch = git.Commit.from_tree(repo, branch_head_tree, "Commit from " + branch, current_parent)
+        commit_for_branch = git.Commit.create_from_tree(repo, branch_head_tree, "Commit from " + branch, current_parent)
         current_parent = [commit_for_branch]
 
-    print(current_parent)    
+    print(current_parent)
+    repo.create_head(target_branch, current_parent[0])
